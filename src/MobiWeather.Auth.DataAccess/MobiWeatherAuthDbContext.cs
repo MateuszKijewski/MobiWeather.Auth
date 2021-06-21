26ea7fe52947ea6f4801b01d0277535e52a37602
Mateusz <mateuszkijewski2307@gmail.com>
2021-06-22 00:28:15 +0200

    Basic project structure configuration

---
+++ src/MobiWeather.Auth.DataAccess/MobiWeatherAuthDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MobiWeather.Auth.Domain.Entities;
+
+namespace MobiWeather.Auth.DataAccess
+{
+    public class MobiWeatherAuthDbContext : DbContext
+    {
+        public MobiWeatherAuthDbContext(DbContextOptions<MobiWeatherAuthDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ src/MobiWeather.Auth.Domain/Contracts/Users/RegisterContract.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MobiWeather.Auth.Domain.Contracts.Users
+{
+    public class RegisterContract
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
